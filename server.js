ccd280089acbc862c7841203fc1700ba7c85591b
jy3266 <70684594+jy3266@users.noreply.github.com>
2022-04-07 20:22:30 -0400

    Update server.js

--- server.js
+++ server.js
@@ -7,9 +7,7 @@ console.log(words);
 var express = require('express');
 var cors = require('cors');
 var app = express();
-var port = process.env.PORT || 8000;
-app.listen(port, () => {console.log(`Starting server at ${port}`);
-});
+var server = app.listen(8000, listening);
 
 function listening(){
     console.log("instening. . .");
