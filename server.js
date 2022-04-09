0daac470d36bdf89dfbbc158e8f59ff24bc6878d
jy3266 <70684594+jy3266@users.noreply.github.com>
2022-04-09 12:48:46 -0400

    Update server.js

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 var fs = require('fs');
-var data = fs.readFileSync('data2.json');
+var data = fs.readFileSync('words.json');
 var words = JSON.parse(data);
 
 console.log(words);
@@ -35,7 +35,7 @@ function addWord(request, response){
     } else {
         words[word] = score;
         var data = JSON.stringify(words, null, 2);
-        fs.writeFile('data2.json',data,finished);
+        fs.writeFile('words.json',data,finished);
         function finished(err){
         console.log('all set.');
         var reply = {
